3aa4a817e10309a04fe821392e0c7eb6f5314b49
T.Kato <goemon12.tc4@gmail.com>
2018-11-03 20:53:29 +0900

    一旦、コミットします。

---
+++ TOKEI-R/DrawVIew.swift
@@ -0,0 +1,74 @@
+//
+//  DrawVIew.swift
+//  TOKEI-R
+//
+//  Created by ただひろ on 2018/11/03.
+//  Copyright © 2018年 Goemon. All rights reserved.
+//
+
+import UIKit
+
+class DrawVIew: UIView {
+
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setFillColor(UIColor.white.cgColor)
+        context?.setStrokeColor(UIColor.black.cgColor)
+        context?.setLineWidth(2.0)
+        
+        context?.beginPath()
+        
+        context?.addArc(
+            center: CGPoint(x: self.center.x, y: self.center.y),
+            radius: self.frame.width / 2,
+            startAngle: 0,
+            endAngle: CGFloat.pi * 2,
+            clockwise: false)
+        
+        context?.addArc(
+            center: CGPoint(x: self.center.x, y: self.center.y),
+            radius: self.frame.width / 2 - 28,
+            startAngle: CGFloat.pi * 2,
+            endAngle: 0,
+            clockwise: true)
+
+        context?.fillPath()
+        
+        context?.beginPath()
+        
+        context?.addArc(
+            center: CGPoint(x: self.center.x, y: self.center.y),
+            radius: self.frame.width / 2 - 30,
+            startAngle: 0,
+            endAngle: CGFloat.pi * 2,
+            clockwise: false)
+        
+        context?.addArc(
+            center: CGPoint(x: self.center.x, y: self.center.y),
+            radius: self.frame.width / 2 - (30 + 28),
+            startAngle: CGFloat.pi * 2,
+            endAngle: 0,
+            clockwise: true)
+        
+        context?.fillPath()
+
+        context?.beginPath()
+        
+        context?.addArc(
+            center: CGPoint(x: self.center.x, y: self.center.y),
+            radius: self.frame.width / 2 - 60,
+            startAngle: 0,
+            endAngle: CGFloat.pi * 2,
+            clockwise: false)
+        
+        context?.addArc(
+            center: CGPoint(x: self.center.x, y: self.center.y),
+            radius: self.frame.width / 2 - (60 + 28),
+            startAngle: CGFloat.pi * 2,
+            endAngle: 0,
+            clockwise: true)
+        
+        context?.fillPath()
+    }
+}
--- TOKEI-R/ViewController.swift
+++ TOKEI-R/ViewController.swift
@@ -10,9 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lblH: [UILabel] = []
+    var lblM: [UILabel] = []
+    var lblS: [UILabel] = []
+    
+    func makeH() {
+        for i in 0 ..< 24 {
+            let tmp = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+            tmp.font = UIFont(name: "Courier", size: 20)
+            tmp.textAlignment = .center
+            tmp.text = "\(i)"
+            self.view.addSubview(tmp)
+            
+            lblH.append(tmp)
+        }
+    }
+    
+    func makeM() {
+        for i in stride(from: 0, to: 60, by: 5) {
+            let tmp = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+            tmp.font = UIFont(name: "Courier", size: 20)
+            tmp.textAlignment = .center
+            tmp.text = "\(i)"
+            self.view.addSubview(tmp)
+
+            lblM.append(tmp)
+        }
+    }
+    
+    func makeS() {
+        for i in stride(from: 0, to: 60, by: 5) {
+            let tmp = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+            tmp.font = UIFont(name: "Courier", size: 20)
+            tmp.textAlignment = .center
+            tmp.text = "\(i)"
+            self.view.addSubview(tmp)
+            
+            lblS.append(tmp)
+        }
+    }
+
+    func dispH() {
+        for i in 0 ..< 24 {
+            let t = CGFloat.pi * 2 / 24 * CGFloat(i)
+            let x = cos(t) * (self.view.frame.width / 2 - 75) + self.view.center.x
+            let y = sin(t) * (self.view.frame.width / 2 - 75) + self.view.center.y
+            lblH[i].center = CGPoint(x: x, y: y)
+            lblH[i].transform = CGAffineTransform(rotationAngle: t)
+        }
+    }
+
+    func dispM() {
+        for i in stride(from: 0, to: 60, by: 5) {
+            let t = CGFloat.pi * 2 / 60 * CGFloat(i)
+            let x = cos(t) * (self.view.frame.width / 2 - 45) + self.view.center.x
+            let y = sin(t) * (self.view.frame.width / 2 - 45) + self.view.center.y
+            lblM[i / 5].center = CGPoint(x: x, y: y)
+            lblM[i / 5].transform = CGAffineTransform(rotationAngle: t)
+        }
+    }
+
+    func dispS() {
+        for i in stride(from: 0, to: 60, by: 5) {
+            let t = CGFloat.pi * 2 / 60 * CGFloat(i)
+            let x = cos(t) * (self.view.frame.width / 2 - 15) + self.view.center.x
+            let y = sin(t) * (self.view.frame.width / 2 - 15) + self.view.center.y
+            lblS[i / 5].center = CGPoint(x: x, y: y)
+            lblS[i / 5].transform = CGAffineTransform(rotationAngle: t)
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        makeH()
+        makeM()
+        makeS()
+        dispH()
+        dispM()
+        dispS()
     }
 
 
