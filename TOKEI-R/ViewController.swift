5f4674821d36127b8993b55004c355eee3fc832b
T.Kato <goemon12.tc4@gmail.com>
2018-11-03 17:25:47 +0900

    Initial Commit

---
+++ TOKEI-R/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TOKEI-R
+//
+//  Created by ただひろ on 2018/11/03.
+//  Copyright © 2018年 Goemon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
